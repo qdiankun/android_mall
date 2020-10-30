ae9290d8e2e570f50109971f50835f6248d44610
diankun.qiu <diankun.qiu@ecovacs.com>
2020-10-30 10:55:47 +0800

    添加轮子哥类库

---
+++ mall/app/src/main/java/com/me/slone/mall/MallApp.java
@@ -0,0 +1,16 @@
+package com.me.slone.mall;
+
+import android.app.Application;
+
+/**
+ * Author：diankun
+ * Time：20-10-29 下午4:52
+ * Description: mall application
+ */
+public class MallApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
