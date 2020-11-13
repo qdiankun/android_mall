3a371b05d02453bb32cfb9742b07d18285000d6b
diankun.qiu <diankun.qiu@ecovacs.com>
2020-11-13 09:49:06 +0800

    添加注册页面

---
+++ mall/app/src/main/java/com/me/slone/mall/ui/activity/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.me.slone.mall.ui.activity;
+
+import com.me.slone.mall.R;
+import com.me.slone.mall.common.MyActivity;
+
+/**
+ * Author：diankun
+ * Time：20-11-13 上午9:06
+ * Description:
+ */
+public class LoginActivity extends MyActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.login_activity;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
