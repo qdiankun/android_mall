f5de48d8e1b1d0341401551ebce51194e1c9fc22
diankun.qiu <diankun.qiu@ecovacs.com>
2020-11-16 15:18:29 +0800

    添加我的页面布局

--- mall/app/src/main/java/com/me/slone/mall/ui/fragment/MeFragment.java
+++ mall/app/src/main/java/com/me/slone/mall/ui/fragment/MeFragment.java
@@ -1,7 +1,11 @@
 package com.me.slone.mall.ui.fragment;
 
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import com.me.slone.mall.R;
 import com.me.slone.mall.common.MyFragment;
+import com.me.slone.mall.http.glide.GlideApp;
 import com.me.slone.mall.ui.activity.HomeActivity;
 
 /**
@@ -10,6 +14,9 @@ import com.me.slone.mall.ui.activity.HomeActivity;
  * Description:
  */
 public class MeFragment extends MyFragment<HomeActivity> {
+
+    private ImageView mAvatarIv;
+    private TextView mNickTv;
 
     public static MeFragment newInstance() {
         return new MeFragment();
@@ -22,7 +29,14 @@ public class MeFragment extends MyFragment<HomeActivity> {
 
     @Override
     protected void initView() {
+        mAvatarIv = findViewById(R.id.iv_avatar);
+        mNickTv = findViewById(R.id.tv_nickname);
 
+        mNickTv.setText("昵称");
+        GlideApp.with(getContext())
+                .load(R.drawable.ic_shop)
+                .circleCrop()
+                .into(mAvatarIv);
     }
 
     @Override
